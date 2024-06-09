1358c62f5941a93f3b83290e7f1a24b25b2911ce
mateicoman <mateicoman01@gmail.com>
2024-06-09 17:16:56 +0300

    refactor?

--- UserMicroservice/Data/UserContext.cs
+++ UserMicroservice/Data/UserContext.cs
@@ -1,6 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using MongoDB.EntityFrameworkCore.Extensions;
+using ProductMicroservice.Models;
+using UserMicroservice.Models;
+
 namespace UserMicroservice.Data;
 
-public class UserContext
+public class UserContext: DbContext
 {
-    
+    private readonly IConfiguration Configuration;
+    public DbSet<User> Users { get; set; }
+    public UserContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMongoDB(Configuration["MongoConnection:ConnectionString"] ?? string.Empty,
+            Configuration["MongoConnection:Database"] ?? string.Empty);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<User>().ToCollection("users");
+    }
 }
--- UserMicroservice/Models/User.cs
+++ UserMicroservice/Models/User.cs
@@ -1,6 +1,15 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace UserMicroservice.Models;
 
 public class User
 {
-    
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? UserId { get; set; }
+    [BsonElement("name")] 
+    public string Name { get; set; } = string.Empty;
+    [BsonElement("address")] 
+    public string Address { get; set; } = string.Empty;
 }
